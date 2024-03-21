38f68cdd84c1717a195c091435421f93b01c47b1
jianwei1 <jianwei1@michaels.com>
2024-03-21 17:53:42 +0800

    update

--- src/main/java/com/raybyte/jstore/JstoreApplication.java
+++ src/main/java/com/raybyte/jstore/JstoreApplication.java
@@ -2,7 +2,6 @@ package com.raybyte.jstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.raybyte.jstore"})
 public class JstoreApplication {
